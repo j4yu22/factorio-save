3c20a4cbd6532bff32e7fb63bda86c2f91f60f4b
jayunderwood <jay.e.underwood@gmail.com>
2026-05-09 14:54:14 -0600

    added save script

---
+++ save.ps1
@@ -0,0 +1,19 @@
+$saveName = "_autosave2.zip"
+
+$factorioSavePath = "$env:APPDATA\Roaming\Factorio\saves\$saveName"
+$repoSavePath = ".\$saveName"
+
+if (-not (Test-Path $factorioSavePath)) {
+    Write-Host "ERROR: Save file not found in Factorio saves folder."
+    Write-Host "Expected: $factorioSavePath"
+    exit 1
+}
+
+Copy-Item $factorioSavePath $repoSavePath -Force
+
+git status
+git add $repoSavePath
+git commit -m "Update Factorio save"
+git push
+
+Write-Host "Copied $saveName from Factorio saves folder and pushed changes to GitHub."
